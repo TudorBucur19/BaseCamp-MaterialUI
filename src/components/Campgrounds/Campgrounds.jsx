50b441384ae4aed40019e014b99de2c6c013abbb
TudorBucur19 <tudor.bucur88@gmail.com>
2021-12-22 15:51:25 +0200

    search by campground name, search bar simplified

--- src/components/Campgrounds/Campgrounds.jsx
+++ src/components/Campgrounds/Campgrounds.jsx
@@ -1,41 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import { FaCampground } from "react-icons/fa";
 
-import { CampgroundsContext } from '../../contexts/CampgroundsContext';
-import CampCard from '../CampCard/CampCard';
-import PrimarySearchAppBar from '../navbar/AppBar';
-import HeaderBanner from '../Common/HeaderBanner';
+import { CampgroundsContext } from 'contexts/CampgroundsContext';
+import CampCard from 'components/CampCard/CampCard';
+import PrimarySearchAppBar from 'components/navbar/AppBar';
 import HeaderStripe from 'components/Common/HeaderStripe';
-import MapBanner from '../MapBanner/MapBanner';
+import MapBanner from 'components/MapBanner/MapBanner';
 
 
 const Campgrounds = () => {
     const { campgroundsList } = useContext(CampgroundsContext);
+    const [searchWord, setSearchWord] = useState({
+        searchWord: '', 
+    });
     const { url } = useRouteMatch();    
+
+    const foundResults = campgroundsList && campgroundsList.filter(
+        result => result.campground.name.toLowerCase().includes(searchWord.searchWord.toLowerCase())
+    );
 
     return ( 
         <Container component="main" disableGutters={true} maxWidth="false">
-            <PrimarySearchAppBar/>
+            <PrimarySearchAppBar {...{setSearchWord}}/>
             <Container maxWidth="lg" mt={4} sx={{marginTop: '30px'}}>
                 <MapBanner campsList={campgroundsList} width="100%" height="400px" icon={FaCampground}/>
                 <HeaderStripe/>
-
+                {foundResults.length ?
                 <Box  py={4}>
                     <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    {campgroundsList &&
-                            campgroundsList.map((campground) => 
-                            <Grid item xs={12} sm={6} md={3} key={campground.id}>
+                    {foundResults &&
+                            foundResults.map((campground) => 
+                            <Grid 
+                            key={campground.id}
+                            item 
+                            xs={12} 
+                            sm={6} 
+                            md={3} 
+                            >
                                 <CampCard {...{campground, url}}/>
                             </Grid>
                             )
                         }                    
                     </Grid>
                 </Box>
-
+                :
+                <Typography>Sorry! No results :(</Typography>
+                }
             </Container>
         </Container>
      );
